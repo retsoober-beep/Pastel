22babbdeb7ba59cba2a659b38d76fd6c988c9632
retsoober-beep <retsoober@gmail.com>
2026-09-01 20:53:58 -0300

    itemTodos - formulação

--- src/components/itemTodos.jsx
+++ src/components/itemTodos.jsx
@@ -2,5 +2,25 @@ import { useTodosStore } from "../store/useTodoStore";
 
 export default function ItemTodo({ todo }) {
     const changeTodo = useTodosStore((state) => state.changeTodo)
-    const deleteTodo = useTodosStore((state) => state.deleteTodo)  
+    const deleteTodo = useTodosStore((state) => state.deleteTodo)
 }
+
+return (
+    <li className={`item-todos ${todo.done ? 'item-todo--done' : ''}`}>
+        <button
+            type="button"
+            className="item-todo-checkbox"
+            aria-label={todo.done ? "Marcar como dependente" : "Marcar como concluído"}
+            onClick={() => changeTodo(todo.id, todo.done)}>{todo.done ? '✔' : ''}
+        </button>
+
+        <span className="item-todo__title" >{todo.title}</span>
+
+        <button
+            type="button"
+            className="item-todo_remove"
+            aria-label="remove todo">
+            Remover
+        </button>
+    </li>
+)
